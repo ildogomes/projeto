47e306daeb52eff1bf9ebace7a504ae7619a81a9
ildogomes <ildo@usp.br>
2014-10-23 12:40:18 -0200

    versao 4

--- index.php
+++ index.php
@@ -7,27 +7,29 @@ function rotas()
 {
     $rota = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
     $paginas	= array('home', 'empresa', 'produtos', 'servicos', 'contato');
-
     $explode = $rota['path'];
+  
+
+  
     $explode = explode('/', $explode);
-    if(empty($explode[2])){
+	
+
+    if(empty($explode[1])){
         include_once( __DIR__."/home.php");
-    }elseif(isset($explode[2]) && in_array($explode[2], $paginas)){
-        include_once(__DIR__."/{$explode[2]}.php");
-    }elseif(isset($explode[2]) && $explode[2] != $paginas){
+    }elseif(isset($explode[1]) && in_array($explode[1], $paginas)){
+        include_once(__DIR__."/{$explode[1]}.php");
+    }elseif(isset($explode[1]) && $explode[1] != $paginas){
         include_once( __DIR__."/naoexiste.php");
     }else{
         include_once( __DIR__."/home.php");
     }
-}
-//echo"<pre>";
-//print_r($explode);
-
+}	 
 
 rotas();
 
 
+	
 
 include_once( __DIR__.'/footer.php');
-//echo "é este alterado";
+
 ?>
